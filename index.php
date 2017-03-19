66f18946e3fdf01fd901358e7faa94aebd3c4dea
Timophet <timophet@yandex.ru>
2017-03-19 12:23:33 +0800

    v

---
+++ index.php
@@ -0,0 +1,98 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Leader Assistance</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+	<header>
+		<nav class="navbar">
+		  <div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+			   <a class="navbar-brand" href="#"><img src="image\logo-min.png" height="40"></a>
+			
+			</div>
+			<div class="nav navbar-nav navbar-right">
+				<form class="navbar-form ">
+					<div class="form-group">
+						<label> 
+							<div class="alert alert-warning" role="alert">
+							Нет подписки 
+							<a href="#" class="alert-link">Купить подписку</a>
+							</div>
+						</label>
+						<label>
+							<div class="alert alert-warning" role="alert">
+								<a href="#" class="alert-link">Профиль</a>
+							</div>
+							
+						</label>	
+						<label>
+							<div class="alert alert-warning" role="alert">
+								<a href="#" class="alert-link">Выйти</a>
+							</div>							
+						</label>												
+					</div>
+					
+				</form>
+			</div>
+			
+			
+			
+		  </div><!-- /.container-fluid -->
+		</nav>
+	</header>
+	
+	
+	
+	<pre>
+		<?php 
+			//echo "fsdfsdfsdfsfsdfsdf";
+			var_dump($_SERVER);
+		?>
+	</pre>
+	<!---div class="container-fluid">
+		<h1> 3 Различных блока </h1>
+		
+		<div class="row">
+			<div class="col-md-2" style="background-color:#ff99cc"> 
+				<div class="row">
+					<div class="col-md-6" style="background-color:#ff9999"> LEFT </div>
+					<div class="col-md-6" style="background-color:#00cc99"> RIGHT </div>
+			
+				</div>
+			
+			</div>
+			<div class="col-md-2" style="background-color:#9900ff"> MIDDLE </div>
+			<div class="col-md-2" style="background-color:#ffcc99"> RIGHT </div>
+			<div class="col-md-2" style="background-color:#ff9999"> LEFT </div>
+			<div class="col-md-2" style="background-color:#99ccff"> MIDDLE </div>
+			<div class="col-md-2" style="background-color:#00cc99"> RIGHT </div>
+		</div>
+		<div class="row">
+			<div class="col-md-3" style="background-color:#ff9999"> LEFT </div>
+			<div class="col-md-3" style="background-color:#99ccff"> MIDDLE </div>
+			<div class="col-md-2" style="background-color:#00cc99"> RIGHT </div>
+			<div class="col-md-2" style="background-color:#ff9999"> LEFT </div>
+			<div class="col-md-2" style="background-color:#99ccff"> MIDDLE </div>
+		</div>
+	</div --->
+	Test
+	<h1>Test</h1>
+</body>
+</html>
+
