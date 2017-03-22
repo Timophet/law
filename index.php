9b702f40958579bcec89a6018d46c9d76afe9a1f
Timophet <timophet@gmail.com>
2017-03-22 23:03:26 +0800

    law site v.0.0.1

--- index.php
+++ index.php
@@ -16,75 +16,88 @@
 
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+	<link href="css/style.css" rel="stylesheet">
+
 </head>
 <body>
 	<header>
 		<nav class="navbar">
-		  <div class="container-fluid">
-			<!-- Brand and toggle get grouped for better mobile display -->
+		  <div class="container">
 			<div class="navbar-header">
-			   <a class="navbar-brand" href="#"><img src="image\logo-min.png" height="40"></a>
-			
+			   <a class="navbar-brand" href="#"><img src="image\logo-min.png" height="30">
+			   	<p> <strong> AsLeader </strong> </p>
+			   </a>
 			</div>
-						
-			<div class="col-md-offset-7 col-md-4">
-				
-					<div class="btn-group" role="group" aria-label="...">
-							<button type="button" class="btn btn-warning">Купить подписку</button>
-							<button type="button" class="btn btn-default">Профиль</button>
-							<button type="button" class="btn btn-default">Выйти</button>
-							
-					</div>
-					 <form class="navbar-form navbar-left">
-						<div class="form-group">
-						  <a href="#">Задать вопрос </a>
-						</div>
-						<span> 8-800-000-00-01</span>
-					  </form>
-					
-			</div>
-			
-			
+				<div class="navbar-right btn-group" role="group">
+					<button type="button" class="btn btn-warning navbar-btn">Купить подписку</button>
+					<button type="button" class="btn btn-default navbar-btn">Профиль</button>
+					<button type="button" class="btn btn-default navbar-btn">Выйти</button>
+				</div>
+
 		  </div><!-- /.container-fluid -->
 		</nav>
+		<nav class="navbar navbar-second">
+		  <div class="container">
+			<form class="navbar navbar-form navbar-right">
+				<div class="form-group">
+			 		<h4> <a href="#">Задать вопрос</a>
+					<span> 8-800-000-00-01 </span>
+					</h4>
+				</div>
+				
+			</form> 
+		</div>
+		</nav>
+
+	<nav class="navbar navbar-main">
+		<div class="container-fluid">
+			<h3 class="navbar-text"> ООО "Торум" </h3>
+			<form class="navbar navbar-form navbar-right">
+				<div class="form-group">
+			 		 <h4 class="navbar-text"> <a href="#">Регистрационные данные</a> </h4>
+				</div>
+				
+			</form> 
+		</div>
+	</nav>
+
+	
+
 	</header>
 	
-	
-	
-	<pre>
-		<?php 
-			//echo "fsdfsdfsdfsfsdfsdf";
-			var_dump($_SERVER);
-		?>
-	</pre>
-	<!---div class="container-fluid">
-		<h1> 3 Различных блока </h1>
-		
-		<div class="row">
-			<div class="col-md-2" style="background-color:#ff99cc"> 
-				<div class="row">
-					<div class="col-md-6" style="background-color:#ff9999"> LEFT </div>
-					<div class="col-md-6" style="background-color:#00cc99"> RIGHT </div>
-			
-				</div>
-			
-			</div>
-			<div class="col-md-2" style="background-color:#9900ff"> MIDDLE </div>
-			<div class="col-md-2" style="background-color:#ffcc99"> RIGHT </div>
-			<div class="col-md-2" style="background-color:#ff9999"> LEFT </div>
-			<div class="col-md-2" style="background-color:#99ccff"> MIDDLE </div>
-			<div class="col-md-2" style="background-color:#00cc99"> RIGHT </div>
+	<div class="container">
+		<ul class="nav nav-tabs nav-justified">
+	  	<li role="presentation" class="active"><a data-toggle="tab" href="#panel1">Органы управления</a></li>
+	  	<li role="presentation"><a data-toggle="tab" href="#panel2">Внешние риски</a></li>
+	  	<li role="presentation"><a data-toggle="tab" href="#panel3">Внутренние риски</a></li>
+		</ul>	
+	</div>
+	<div class="container">
+		<div class="tab-content">
+	  		<div id="panel1" class="tab-pane fade in active">
+	    		<h3>Общее собрание участников</h3>
+	    		<h3>Совет директоров</h3>
+	    		<h3>Единоличный исполнительный орган</h3>
+		  	</div>
+		  	<div id="panel2" class="tab-pane fade">
+		    	<h3>Государственные органы</h3>
+		    	<ul> 
+		    		<li> Федеральная налоговая служба </li>
+		    		<li> Роспотребнадзор </li>
+		    	</ul>
+		    	<h3>Контрагенты</h3>
+		    	
+		  	</div>
+		  	<div id="panel3" class="tab-pane fade">
+		    	<h3>Работники организации</h3>
+		    	
+		  	</div>
+		  	
 		</div>
-		<div class="row">
-			<div class="col-md-3" style="background-color:#ff9999"> LEFT </div>
-			<div class="col-md-3" style="background-color:#99ccff"> MIDDLE </div>
-			<div class="col-md-2" style="background-color:#00cc99"> RIGHT </div>
-			<div class="col-md-2" style="background-color:#ff9999"> LEFT </div>
-			<div class="col-md-2" style="background-color:#99ccff"> MIDDLE </div>
-		</div>
-	</div --->
-	Test
-	<h1>Test</h1>
+	</div>
+
+
 </body>
 </html>
 
