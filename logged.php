5f30a74a41af70f3b9e070f54290e30499a218c1
Timophet <timophet@yandex.ru>
2017-03-26 15:05:35 +0800

    2

---
+++ logged.php
@@ -0,0 +1,106 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Leader Assistance</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+	<link href="css/style.css" rel="stylesheet">
+	<link rel="shortcut icon" href="/image/favicon.ico" type="image/x-icon">
+</head>
+<body>
+	<header>
+		<nav class="navbar">
+		  <div class="container">
+			<div class="navbar-header">
+			   <a class="navbar-brand" href="#"><img src="image\logo-min.png" height="30">
+			   	<p> <strong> AsLeader </strong> </p>
+			   </a>
+			</div>
+				<div class="navbar-right btn-group" role="group">
+					<button type="button" class="btn btn-warning navbar-btn">Купить подписку</button>
+					<button type="button" class="btn btn-default navbar-btn">Профиль</button>
+					<button type="button" class="btn btn-default navbar-btn">Выйти</button>
+				</div>
+
+		  </div><!-- /.container-fluid -->
+		</nav>
+		<nav class="navbar navbar-second">
+		  <div class="container">
+			<form class="navbar navbar-form navbar-right">
+				<div class="form-group">
+			 		<h4> <a href="#">Задать вопрос</a>
+					<span> 8-800-000-00-01 </span>
+					</h4>
+				</div>
+				
+			</form> 
+		</div>
+		</nav>
+
+	<nav class="navbar navbar-main">
+		<div class="container-fluid">
+			<h3 class="navbar-text"> ООО "Торум" </h3>
+			<form class="navbar navbar-form navbar-right">
+				<div class="form-group">
+			 		 <h4 class="navbar-text"> <a href="#">Регистрационные данные</a> </h4>
+				</div>
+				
+			</form> 
+		</div>
+	</nav>
+
+	
+
+	</header>
+	
+	<div class="container">
+		<ul class="nav nav-tabs nav-justified">
+	  		<li role="presentation" class="active"><a data-toggle="tab" href="#panel1">Органы управления</a></li>
+	  		<li role="presentation"><a data-toggle="tab" href="#panel2">Внешние риски</a></li>
+	  		<li role="presentation"><a data-toggle="tab" href="#panel3">Внутренние риски</a></li>
+			
+		</ul>	
+	</div>
+	<div class="container">
+		
+		<div class="tab-content">
+	  		<div id="panel1" class="tab-pane fade in active">
+	    		<h3>Общее собрание участников</h3>
+	    		<h3>Совет директоров</h3>
+	    		<h3>Единоличный исполнительный орган</h3>
+		  	</div>
+		  	<div id="panel2" class="tab-pane fade">
+		    	<h3>Государственные органы</h3>
+		    	<ul> 
+		    		<li> Федеральная налоговая служба </li>
+		    		<li> Роспотребнадзор </li>
+		    	</ul>
+		    	<h3>Контрагенты</h3>
+		    	
+		  	</div>
+		  	<div id="panel3" class="tab-pane fade">
+		    	<h3>Работники организации</h3>
+		    	
+		  	</div>
+		  	
+		  	
+		</div>
+	</div>
+
+
+</body>
+</html>
+
